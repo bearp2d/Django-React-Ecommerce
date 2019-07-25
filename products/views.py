af537ee291035fb93d7ea7bf38d817d8e2a3d8af
bearp2d <bearp2d@gmail.com>
2019-07-25 19:37:50 +0430

    Add pagintaion and ordering to product list view

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.filters import OrderingFilter
 
 from .serializers import ProductListSerializer, ProductDetailSerializer
 from .models import Product
+from .pagination import ProductPagination
 
 
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
+    pagination_class = ProductPagination
+    filter_backends = (OrderingFilter, )
+    ordering_fields = ('created_at', 'sale_count', 'price')
 
 
 class ProductDetailView(RetrieveAPIView):
