734fcd8e1fe4d3999919ec6c811f790d31320be1
bearp2d <bearp2d@gmail.com>
2019-09-02 18:45:22 +0430

    Fix national code validator message

--- accounts/validators.py
+++ accounts/validators.py
@@ -3,12 +3,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 def validate_national_code(code):
+    message = "Invalid national code."
     if len(code) != 10:
         raise ValidationError(
-            _('%(code)s is not valid national code.'), params={'code': code})
+            _(message), params={'code': code})
     if code == "0000000000" or code == "1111111111" or code == "2222222222" or code == "3333333333" or code == "4444444444" or code == "5555555555" or code == "6666666666" or code == "7777777777" or code == "8888888888" or code == "9999999999":
         raise ValidationError(
-            _('%(code)s is not valid national code.'), params={'code': code})
+            _(message), params={'code': code})
     ctrl_code = int(code[-1])
     code = list(code)
     del code[-1]
@@ -20,10 +21,10 @@ def validate_national_code(code):
     if total < 2:
         if ctrl_code != total:
             raise ValidationError(
-                _('%(code)s is not valid national code.'), params={'code': code})
+                _(message), params={'code': code})
     if ctrl_code != 11 - total:
         raise ValidationError(
-            _('%(code)s is not valid national code.'), params={'code': code})
+            _(message), params={'code': code})
 
 
 phone_number_or_email_reg = '^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$|^09\d{9}$'
