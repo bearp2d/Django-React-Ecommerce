90804f405fb05f69c1f7cca4c13df6148143a65d
bearp2d <bearp2d@gmail.com>
2019-10-05 14:32:57 +0330

    Create fovorite products model whene user registered

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from products.models import Product
 
@@ -25,11 +27,27 @@ class Address(models.Model):
 
 
 class FavoritesProducts(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, related_name='products')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='favorite_products')
+    products = models.ManyToManyField(
+        Product, related_name='products', blank=True)
 
     class Meta:
         verbose_name_plural = 'Favorites Products'
 
     def __str__(self):
         return self.user.username
+
+# Each user should be have favorite products
+# When user registered create favorite products model with this user
+
+
+@receiver(post_save, sender=User)
+def create_fvorite_products(sender, instance, created, **kwargs):
+    if created:
+        FavoritesProducts.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_fvorite_products(sender, instance, **kwargs):
+    instance.favorite_products.save()
