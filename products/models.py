a22d5b3afafab66d0adbc96790743f6b2cb072aa
bearp2d <bearp2d@gmail.com>
2019-10-18 12:05:34 +0330

    Fix products photos location

--- products/models.py
+++ products/models.py
@@ -49,10 +49,14 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     slug = AutoSlugField(populate_from='title')
     photo_main = models.ImageField(upload_to='product_photos/%Y/%m/%d/')
-    photo_1 = models.ImageField(blank=True, null=True)
-    photo_2 = models.ImageField(blank=True, null=True)
-    photo_3 = models.ImageField(blank=True, null=True)
-    photo_4 = models.ImageField(blank=True, null=True)
+    photo_1 = models.ImageField(
+        upload_to='product_photos/%Y/%m/%d/', blank=True, null=True)
+    photo_2 = models.ImageField(
+        upload_to='product_photos/%Y/%m/%d/', blank=True, null=True)
+    photo_3 = models.ImageField(
+        upload_to='product_photos/%Y/%m/%d/', blank=True, null=True)
+    photo_4 = models.ImageField(
+        upload_to='product_photos/%Y/%m/%d/', blank=True, null=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_price = models.DecimalField(
