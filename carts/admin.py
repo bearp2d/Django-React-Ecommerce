10a7f02fde7adceac28e990ba5f9ebbab9b3fbcb
bearp2d <bearp2d@gmail.com>
2019-11-28 15:25:55 +0330

    Customize carts app Cart and CartItem model admin

--- carts/admin.py
+++ carts/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 
 from .models import Cart, CartItem
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('id', 'user', 'total_price', 'total_customer_profit', 'items_count',
+                    'ordered', 'created_at')
+    list_display_links = ('id', 'user')
+    list_filter = ('ordered', 'created_at')
+    list_per_page = 25
+    search_fields = ('user__phone_number', 'user__email',
+                     'items__product__title')
+    readonly_fields = ('ordered',)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cart', 'product', 'size', 'quantity',
+                    'total_price', 'total_customer_profit')
+    list_display_links = ('id', 'cart')
+    list_per_page = 25
+    search_fields = ('cart__user__phone_number',
+                     'cart__user__email', 'product__title')
+    readonly_fields = ('quantity',)
--- carts/models.py
+++ carts/models.py
@@ -33,6 +33,9 @@ class Cart(models.Model):
     ordered = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-created_at',)
+
     def __str__(self):
         return self.user.username
 
