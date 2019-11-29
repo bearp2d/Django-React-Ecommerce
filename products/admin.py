b49bc4f3d302ef2888e7f115fe904172a1fea849
bearp2d <bearp2d@gmail.com>
2019-11-29 12:11:06 +0330

    Customize products app Size and Product model admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 
 from .models import Product, Size
 
-admin.site.register(Product)
-admin.site.register(Size)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('id', 'title', 'price',
+                    'discount_price', 'available', 'active', 'sale_count', 'created_at')
+    list_display_links = ('id', 'title')
+    list_editable = ('active',)
+    list_filter = ('active', 'created_at')
+    list_per_page = 25
+    search_fields = ('title', 'price', 'description')
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('id', '__str__', 'size', 'min_size',
+                    'max_size', 'available_count')
+    list_display_links = ('id', '__str__')
+    list_editable = ('size', 'min_size', 'max_size', 'available_count')
+    list_filter = ('size',)
+    list_per_page = 25
+    search_fields = ('size', 'min_size', 'max_size', 'available_count')
