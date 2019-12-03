c60113bdb3f7ae80f6db6e83975b01f5abb42866
bearp2d <bearp2d@gmail.com>
2019-12-03 18:15:08 +0330

    Use icons for auth links instead of buttons

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -7,9 +7,11 @@ import Badge from "@material-ui/core/Badge";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
+import IconButton from "@material-ui/core/IconButton";
 import CartIcon from "@material-ui/icons/ShoppingCart";
 
 import Search from "../products/Filters/Search";
+import ProfileIcon from "@material-ui/icons/Person";
 
 const useStyles = makeStyles(theme => ({
   rightItems: {
@@ -29,17 +31,14 @@ const Header = () => {
 
   const authNav = (
     <div className={classes.rightItems}>
-      <Button component={RouterLink} to="/cart" color="inherit">
+      <IconButton component={RouterLink} to="/profile" color="inherit">
+        <ProfileIcon />
+      </IconButton>
+      <IconButton component={RouterLink} to="/cart" color="inherit">
         <Badge color="secondary" badgeContent={user && user.cart_items_count}>
           <CartIcon />
         </Badge>
-      </Button>
-      <Button component={RouterLink} to="/profile" color="inherit">
-        Profile
-      </Button>
-      <Button component={RouterLink} to="/logout" color="inherit">
-        Logout
-      </Button>
+      </IconButton>
     </div>
   );
 
@@ -55,32 +54,30 @@ const Header = () => {
   );
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Link
-            component={RouterLink}
-            to="/"
-            variant="h6"
-            color="inherit"
-            className={classes.title}
-            underline={"none"}
-          >
-            Home
-          </Link>
-          <Button
-            className={classes.button}
-            component={RouterLink}
-            to="/products"
-            color="inherit"
-          >
-            Products
-          </Button>
-          <Search />
-          {isAuthenticated === false ? guestNav : authNav}
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Link
+          component={RouterLink}
+          to="/"
+          variant="h6"
+          color="inherit"
+          className={classes.title}
+          underline={"none"}
+        >
+          Home
+        </Link>
+        <Button
+          className={classes.button}
+          component={RouterLink}
+          to="/products"
+          color="inherit"
+        >
+          Products
+        </Button>
+        <Search />
+        {isAuthenticated === false ? guestNav : authNav}
+      </Toolbar>
+    </AppBar>
   );
 };
 
