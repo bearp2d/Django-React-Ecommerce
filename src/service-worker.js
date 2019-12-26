b375acc6d807b1366e8f2b9777976bf3eec5d95b
bearp2d <bearp2d@gmail.com>
2019-12-26 09:30:54 +0330

    Add navigation route to service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,6 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+// Single app navigation route
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html")
+);
