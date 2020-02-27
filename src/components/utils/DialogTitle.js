bd5af585b33eb63014cb179c4ab8fe59e78db231
bearp2d <bearp2d@gmail.com>
2020-02-27 12:13:54 +0330

    Change dialog title close icon color

--- src/components/utils/DialogTitle.js
+++ src/components/utils/DialogTitle.js
@@ -13,8 +13,7 @@ const useStyles = makeStyles(theme => ({
   closeButton: {
     position: "absolute",
     right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500]
+    top: theme.spacing(1)
   }
 }));
 
